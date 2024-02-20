6dc7d876f042efc162c71288d0aafbbcea2df298
Dimitry364 <g4779@yandex.ru>
2024-02-21 06:02:14 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 
-const { loginValidator, createUserValidator } = require('../middlewares/validation');
-const { login, createUser } = require('../controllers/users');
-
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const login = require('./signin');
+const createUser = require('./signup');
 const auth = require('../middlewares/auth');
 
 const NotFoundError = require('../errors/NotFoundError');
@@ -15,8 +14,8 @@ router.get('/crash-test', () => {
   }, 0);
 });
 
-router.post('/signin', loginValidator, login);
-router.post('/signup', createUserValidator, createUser);
+router.use('/signin', login);
+router.use('/signup', createUser);
 
 router.use(auth);
 router.use('/users', usersRouter);
