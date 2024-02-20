a8886c2a5a1a2d0fa9c2b85bbc6a7774411cf839
Dimitry364 <g4779@yandex.ru>
2024-02-21 05:05:20 +0700

    update

--- app.js
+++ app.js
@@ -2,34 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const routes = require('./routes');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
-const { loginValidator, createUserValidator } = require('./middlewares/validation');
+
+const cors = require('./middlewares/cors');
 const { mongodb } = require('./utils/mongodb');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+const { limiter } = require('./middlewares/rateLimit');
 
 const { PORT = 3000, DB_CONN = mongodb } = process.env;
 const app = express();
 
-app.use(requestLogger);
-
 app.use(express.json());
 app.use(cors);
+app.use(helmet());
+app.use(requestLogger);
+app.use(limiter);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадет');
-  }, 0);
-});
-
-app.post('/signin', loginValidator, login);
-app.post('/signup', createUserValidator, createUser);
-
-app.use(auth);
-app.use(routes);
+app.use('/', routes);
 
 app.use(errorLogger);
 app.use(errors());
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
+
+const { loginValidator, createUserValidator } = require('../middlewares/validation');
+const { login, createUser } = require('../controllers/users');
+
 const usersRouter = require('./users');
-const movieRouter = require('./movies');
+const moviesRouter = require('./movies');
+const auth = require('../middlewares/auth');
+
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадет');
+  }, 0);
+});
+
+router.post('/signin', loginValidator, login);
+router.post('/signup', createUserValidator, createUser);
+
+router.use(auth);
 router.use('/users', usersRouter);
-router.use('/movies', movieRouter);
+router.use('/movies', moviesRouter);
 
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
